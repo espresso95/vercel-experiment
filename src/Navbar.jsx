68af7e11d9b2e8c6836d7eae492a5bd27ddfd5da
Espresso95 <88472285+espresso95@users.noreply.github.com>
2025-08-04 10:50:33 -0400

    feat: add sliding sidebar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,11 +4,22 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
-function Navbar() {
+function Navbar({ onMenuClick }) {
   return (
     <AppBar position="static">
       <Toolbar>
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          onClick={onMenuClick}
+          sx={{ mr: 2 }}
+        >
+          <MenuIcon />
+        </IconButton>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           My Site
         </Typography>
